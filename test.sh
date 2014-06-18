6951dc6e209b745829a6e277320df66073be41e7
Andy May <ajmay81@gmail.com>
2014-06-18 09:52:27 +0100

    initial commit

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# USAGE
+#
+#   ./test.sh
+#
+# DESCRIPTION
+#
+#   Generate and run a configure file to test the
+#   MAY_FC_MODULE_FILENAME_FORMAT macro. All options passed to this
+#   script are passed onto configure, so autoconf options or environment
+#   variables can be used in the usual way to set the compiler.
+#
+# LICENSE
+#
+#   Copyright (c) 2014 Andy May <ajmay81@gmail.com>
+#
+#   Copying and distribution of this file, with or without modification, are
+#   permitted in any medium without royalty provided the copyright notice
+#   and this notice are preserved. This file is offered as-is, without any
+#   warranty.
+
+cat << _EOF_ > configure.ac
+AC_INIT(test,test)
+m4_include([may_fc_module_filename_format.m4])
+MAY_FC_MODULE_FILENAME_FORMAT()
+_EOF_
+autoconf -Wall configure.ac > configure
+rm -rf autom4te.cache configure.ac
+chmod +x configure
+./configure ${*}
+rm -f configure config.log config.status
